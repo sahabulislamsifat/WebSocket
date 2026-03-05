2b8842e888d1e251191f2f1faa2e0cad1d042dc3
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2026-03-06 00:35:04 +0600

    create a basic chat app

---
+++ chat-app/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('chat message', (msg) => {
+    console.log('Message:', msg);
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
